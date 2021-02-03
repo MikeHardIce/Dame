07a238a4a85bf2f5cb93ebcbbe791f5e1cd294a5
OgreBelly <ogrebelly@protonmail.com>
2021-02-03 20:31:00 +0000

    some logic for possible moves

--- src/dame/game_logic.clj
+++ src/dame/game_logic.clj
@@ -1,9 +1,31 @@
 (ns dame.game-logic)
 
-(defn possible-moves
-  "Returns a vector consiting of possible next moves"
-  [game x y]
+(defn within-board? 
+  [game [x y]]
+  (let [y-max (count game)
+        x-max (count (game 0))]
+    (and (< y y-max) (< x x-max)
+         (>= y 0) (>= x 0))))
+
+(defn possible-moves-normal
+  [game x y player]
+  (let [direction (if (= player :player1) -1 1)
+        pos-moves [[(inc x) (+ y direction)] [(dec x) (+ y direction)]]
+        moves (filter #(within-board? game %) pos-moves)]
+    moves))
+
+(defn possible-moves-dame
+  [game x y player]
   [[x y]])
+
+(defn possible-moves
+  "Returns a vector consisting of possible next moves"
+  [game x y]
+  (when (within-board? game [x y])
+    (when-let [stone (seq ((game y) x))]
+      (if (< (count stone) 2)
+        (possible-moves-normal game x y (nth stone 0))
+        (possible-moves-dame game x y (nth stone 0))))))
 
 (defn next 
   "Returns a new game with the transition [x0 y0] -> [x y] applied"
