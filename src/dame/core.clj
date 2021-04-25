ab7837b507b3fd08119d2dcac55c5379eac4980b
MikeHardIce <mikehardice@gmail.com>
2021-04-25 18:13:18 +0100

    added turn based system and restricted moves

--- src/dame/core.clj
+++ src/dame/core.clj
@@ -14,6 +14,8 @@
 
 (def current-player (atom '(:player1 :player2)))
 
+(def restrict-moves (atom []))
+
 (defn count-stones
   [game player]
   (let [game (mapcat identity game)
@@ -59,13 +61,19 @@
 
 (defmethod board/game :tile-clicked
   [_ current-board coord]
-  (let [x (first coord)
+  ;; either restricted moves is empty or the current clicked tile
+  ;; belongs to a restricted move
+  (when (or (not (seq @restrict-moves))
+            (some #(= coord %) @restrict-moves))
+    (let [x (first coord)
         y (second coord)
         moves (logic/possible-moves @game x y)
         tile ((@game y) x)
         player (first (:player tile))
-        moves (if (= player (first @current-player)) moves [])]
+        moves (if (= player (first @current-player)) moves [])
+        cnt-opponent-stones-before (count-stones @game (second @current-player))]
     (if (and (seq tile) (:selected tile) (= (:selection-color tile) :yellow-green))
+      ;; find the stone that was selected previously (jump from :green -> :yellow-green)
       (let [near-stones (for [xi (range 8)
                               yi (range 8)]
                           [xi yi (:selection-color ((@game yi) xi))])
@@ -74,10 +82,22 @@
                               near-stones)]
         (println " [" x0 " " y0 "] [" x " " y "] ")
         (swap! game logic/next-game [x0 y0] [x y])
-        (swap! game unmark-all))
-      (do 
+        (swap! game unmark-all)
+        (let [potential-restricted-moves (filter #(seq (logic/stones-on-the-way @game [x y] % (second @current-player))) moves)]
+          (if (and (< (count-stones @game (second @current-player)) cnt-opponent-stones-before)
+                   (seq potential-restricted-moves))
+            (do ;; an opponent stone was removed and the player can remove another stone
+              (swap! restrict-moves concat potential-restricted-moves)
+              (swap! game mark-moves potential-restricted-moves)
+              (swap! game mark-stone x y))
+            (do ;; if nothing happened, then hand the turn over to the next player
+              (swap! current-player reverse)
+              (reset! restrict-moves []))))))
+      (do ;; the player selected a tile that wasn't marked as :yellow-green 
+        ;; (outside of :green -> :yellow-green move)
         (swap! game unmark-all)
         (swap! game mark-moves moves)
-        (swap! game mark-stone x y)))
+        (swap! game mark-stone x y))))
+    ;; redraw the game now with the potentially new state of the game
     (board/draw-game current-board @game)
-    (board/show-player-label current-board (first @current-player))))
+    (board/show-player-label current-board (first @current-player)))
